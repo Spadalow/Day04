1efedf0291f83e44630632906b41f54ea91c8fbe
Spadalow <abdullatif.skejic@gmail.com>
2021-03-11 22:58:45 +0100

    Myquotient

---
+++ ex06/src/index.js
@@ -0,0 +1,15 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+var numOne= numOne;
+var numTwo = numTwo ;
+var Myquotient = numOne / numTwo;
+    // Only change code above this line
+    return Myquotient;
+
+}
+
+console.log(main(5.0, 2.0)); 
+console.log(main(16.5, 5.5));//change this line
+console.log(main(102.0, 25.5));
+
+module.exports = main;
